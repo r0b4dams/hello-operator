84d46e3759da3e5ae2176b8f4ea3bdad774fc70b
Robert Adams <r0b4dams@gmail.com>
2024-01-15 22:25:30 -0800

    Formatting

--- cmd/main.go
+++ cmd/main.go
@@ -90,8 +90,8 @@ func main() {
 	}
 
 	if err = (&controller.HelloWorldReconciler{
-		Client: mgr.GetClient(),
-		Scheme: mgr.GetScheme(),
+		Client:   mgr.GetClient(),
+		Scheme:   mgr.GetScheme(),
 		Recorder: mgr.GetEventRecorderFor("helloworld-controller"),
 	}).SetupWithManager(mgr); err != nil {
 		setupLog.Error(err, "unable to create controller", "controller", "HelloWorld")
