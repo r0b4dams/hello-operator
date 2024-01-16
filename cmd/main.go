940fb98e6c2e9bdf2ccf010580a4bc856287e7e3
Robert Adams <r0b4dams@gmail.com>
2024-01-16 00:20:09 -0800

    Update image

--- cmd/main.go
+++ cmd/main.go
@@ -97,6 +97,14 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "HelloWorld")
 		os.Exit(1)
 	}
+	if err = (&controller.HelloWorldReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+		Recorder: mgr.GetEventRecorderFor("helloworld-controller"),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "HelloWorld")
+		os.Exit(1)
+	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
